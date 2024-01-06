f584657ebd888fab05a6b14170f6b3f83736948d
stymjhrbd <satyamjhrbd@gmail.com>
2024-01-07 01:35:45 +0530

    made chat component

--- src/app/components/ui/form.tsx
+++ src/app/components/ui/form.tsx
@@ -2,9 +2,10 @@
 
 export default function Form() {
     return (
-        <form className="p-6 fixed bottom-0 left-0 w-full">
+        <form className="p-6 fixed bottom-0 left-0 w-full ">
             <div className="flex">
-                <input type="text" name="message" placeholder="Type your message..." className="flex-grow py-2 px-4 outline-none" />
+                <input type="text" name="message" placeholder="Type your message..." className="flex-grow py-2 px-4 outline-none " />
+                <button type="submit" className="bg-teal-500 hover:bg-teal-600 text-white py-2 px-4 rounded-full">Send</button>
             </div>
         </form>
     )
